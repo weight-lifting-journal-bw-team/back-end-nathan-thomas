1c3abfe4ec74522452660c529d7db855a899c9de
Nathan Thomas <nwthomas@me.com>
2019-03-08 16:21:50 -0800

    Flesh out user migration table

--- database/migrations/20190225134140_users.js
+++ database/migrations/20190225134140_users.js
@@ -1,8 +1,26 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", tbl => {
-    tbl.increments("id");
+    // Unique user id
+    tbl.increments("user_id");
+
+    // User profile
     tbl.string("username", 128).notNullable();
-    tbl.string("password", 128).notNullable();
+    tbl.string("password", 256).notNullable();
+    tbl.string("first_name", 128).notNullable();
+    tbl.string("last_name", 128).notNullable();
+    tbl.string("email", 128).notNullable();
+    tbl.string("address", 256).notNullable();
+    tbl.string("city", 256).notNullable();
+    tbl.string("state", 128).notNullable();
+    tbl
+      .integer("zip_code")
+      .unsigned()
+      .notNullable();
+
+    // Tracks account creation/update
+    tbl.timestamps(true, true);
+
+    // Sets unique username to user
     tbl.unique("username", "uq_user_username");
   });
 };
